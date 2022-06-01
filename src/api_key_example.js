be7cc5c32e660a8d94949961e2fbd2ea39f69a21
jose <joseluis836@hotmail.com>
2022-06-01 11:08:25 -0500

    Se agrego la llave a nuestra aplicación

--- src/api_key_example.js
+++ src/api_key_example.js
@@ -1,3 +1,7 @@
 //Cambia el nombre de este archivo a api_key.js
 //Debes crear en él, el nombre de tu llave
+//La llave la solicitas en la pagina de la api
+//https://www.themoviedb.org/settings/api
 
+//Ejemplo
+//const Api_key = 4e0r74334dsf970b3er0ad46c843e5f4;
